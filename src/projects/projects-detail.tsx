c28402d8c150eead159c855c74d52dc172f382e9
Richie <richardokonicha@gmail.com>
2021-10-30 17:50:49 +0100

    project detail component edit

--- src/projects/projects-detail.tsx
+++ src/projects/projects-detail.tsx
@@ -1,20 +1,36 @@
 import { useParams } from "react-router-dom";
 import { useFirestore, useFirestoreDocData } from 'reactfire';
 import { doc, enableIndexedDbPersistence, increment, updateDoc, orderBy, query, addDoc } from 'firebase/firestore';
-import { Box, Progress, Container } from "@chakra-ui/react";
+import { Box, Progress, Container, HStack, Text, Tag, Switch, Spacer } from "@chakra-ui/react";
 
 function ProjectDetail() {
     const { projectId } = useParams<{ projectId: string }>()
     const firestore = useFirestore();
     const ref = doc(firestore, 'projects', projectId);
     const { status, data } = useFirestoreDocData(ref);
+    console.log(data)
     return (
         <Container maxW="container.xl" pt={4}>
             {status === "loading" && (
                 <Progress isIndeterminate={status === 'loading'} isAnimated />
             )}
             {status === "success" && (
-                <Box>{data?.description}</Box>
+                <Box shadow="md" p={4}>
+                    <HStack>
+                        <Text textTransform="capitalize" >{data?.name}</Text>
+                        <Tag size='sm' variant="solid" colorScheme="blueprint">
+                            Live
+                        </Tag>
+                        <Switch id="turn on" />
+                        <Spacer />
+                        <Tag size='sm' variant="solid" colorScheme="teal">
+                            Healthy
+                        </Tag>
+                    </HStack>
+                    <Text>{data?.heroku_alias || "footprint"}</Text>
+                    <Text textTransform="capitalize">{data?.description}</Text>
+
+                </Box>
             )}
         </Container>
     )
