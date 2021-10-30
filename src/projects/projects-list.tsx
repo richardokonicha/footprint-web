c755bb05ec635c846726b6687f0237c5fb46b3fe
Richie <richardokonicha@gmail.com>
2021-10-30 15:01:41 +0100

    added project list and create project

--- src/projects/projects-list.tsx
+++ src/projects/projects-list.tsx
@@ -1,54 +1,133 @@
-import { Box, Flex, Heading, Tag, Text, HStack } from '@chakra-ui/react';
+import { Box, Flex, Modal, useToast, ModalContent, ModalFooter, ModalCloseButton, ModalBody, ModalOverlay, IconButton, useDisclosure, Wrap, Center, WrapItem, Stack, Grid, Heading, Spacer, Tag, Text, HStack, Progress, ModalHeader, Button, Input } from '@chakra-ui/react';
 import { firestore } from '../services/firebaseClient'
 import { collection, addDoc, getDocs, doc } from "firebase/firestore";
 import { useFirestoreDocData, useFirestore, useFirebaseApp, useFirestoreCollectionData } from 'reactfire';
+import { PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icons'
+import { Formik } from 'formik'
+import {
+    Link,
+} from "react-router-dom";
 
 
 function Projects() {
-    const projectRef = collection(useFirestore(), 'projects');
-    const { status, data: value } = useFirestoreCollectionData(projectRef);
-
-    // const addProject = () => {
-    //     addDoc(db.collection('projects'), {
-    //         name: 'New Project',
-    //         description: 'New Project Description',
-    //     })
-    // }
-
-    // Add a new document with a generated id.
-    // const docRef = await addDoc(collection(db, "cities"), {
-    //     name: "Tokyo",
-    //     country: "Japan"
-    // });
-    // console.log("Document written with ID: ", docRef.id);
-
+    const toast = useToast()
+    const firestore = useFirestore()
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const projectRef = collection(firestore, 'projects')
+    const { status, data: value, error } = useFirestoreCollectionData(projectRef)
+    const addProject = ({ name, description }: { name: string, description: string }) => {
+        addDoc(projectRef, {
+            name: name,
+            description: description,
+        }).then(
+            (value) => {
+                toast({
+                    title: "service created.",
+                    description: `${name} created`,
+                    status: "success",
+                    duration: 2000,
+                    isClosable: true,
+                })
+            }
+        )
+        onClose()
+    }
     return (
-
         <Box>
-            {/* {error && <strong>Error: {JSON.stringify(error)}</strong>} */}
-            {/* {loading && <span>Collection: Loading...</span>} */}
-            {value && (
-                <span>
-                    Collection:{' '}
-                    {value.map((doc) => (
-                        <Flex mt={8} p={4} key={doc.name}>
+            {error && <strong>Error: {JSON.stringify(error)}</strong>}
+            <Progress isIndeterminate={status === 'loading'} isAnimated />
+            {status === "success" && (
+                <Flex mt={8} p={4} key={doc.name} flexWrap='wrap'>
+                    {value.map((doc, index) => (
 
-                            <Box boxShadow="md" p="6" rounded="md" bg="#F8F8F8">
-                                <HStack>
-                                    <Heading>{doc.name}</Heading>
-                                    <Tag size='md' variant="solid" colorScheme="blueprint">
+                        <Link to={`/project/projectid=${index}`}>
+                            <Box
+                                key={index}
+                                boxShadow="md"
+                                flexGrow={1}
+                                minW={64}
+                                m={4}
+                                p={6}
+                                rounded="xs"
+                                bg="#F8F8F8"
+                                _hover={{
+                                    background: "white",
+                                    color: "teal.500",
+                                }}>
+                                <HStack mb={14}>
+                                    <Text fontSize="md" textTransform="capitalize">{doc.name}</Text>
+                                    <Spacer />
+
+                                    <Tag size='sm' variant="solid" colorScheme="blueprint">
                                         Live
                                     </Tag>
                                 </HStack>
+                                <Spacer />
 
-                                <Text>{doc.heroku_alias}</Text>
-                                <Text>Created on 6/6/78</Text>
+                                <Text mb={1} fontSize="xs">{doc.heroku_alias}</Text>
+                                <Text color="gray.500" fontSize="xs">Created on 6/6/78</Text>
                             </Box>
-                        </Flex>
-                    ))}
-                </span>
-            )}
+                        </Link>
 
+                    ))}
+                    <Center boxShadow="md" m={4} rounded="sm" bg="#F8F8F8" >
+                        <IconButton onClick={onOpen} fontSize={24} p={8} w={24} minW={64} height="100%" colorScheme="gray" aria-label="create new" icon={<AddIcon />} />
+
+                    </Center>
+                    <Modal isOpen={isOpen} onClose={onClose}>
+                        <ModalOverlay />
+                        <Formik
+                            initialValues={{ name: '', description: '' }}
+                            onSubmit={(values, { setSubmitting }) => {
+                                addProject(values)
+                                setSubmitting(false);
+                            }}
+                        >
+                            {({
+                                values,
+                                errors,
+                                touched,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                                isSubmitting,
+                            }) => (
+                                <form onSubmit={handleSubmit}>
+                                    <ModalContent>
+                                        <ModalHeader>Create new service</ModalHeader>
+                                        <ModalCloseButton />
+                                        <ModalBody>
+                                            <Box>
+                                                <Input type="text"
+                                                    name="name"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.name}
+                                                    placeholder="Name" mb={4} />
+                                                {errors.name && touched.name && errors.name}
+                                                <Input type="text"
+                                                    name="description"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.description}
+                                                    placeholder="description" />
+                                                {errors.description && touched.description && errors.description}
+                                            </Box>
+                                        </ModalBody>
+                                        <ModalFooter>
+                                            <Button onClick={onClose} variant="ghost">Close</Button>
+                                            <Button type="submit" disabled={isSubmitting} colorScheme="blue" mr={3} >
+                                                Create
+                                            </Button>
+                                        </ModalFooter>
+                                    </ModalContent>
+                                </form>
+                            )}
+                        </Formik>
+                    </Modal>
+                </Flex>
+            )
+            }
         </Box >
 
     )
